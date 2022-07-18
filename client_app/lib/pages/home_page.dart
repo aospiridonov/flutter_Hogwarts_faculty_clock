e9346b2d665c543794f2838da0bcbd876ff0f862
aospiridonov <aospiridonov@gmail.com>
2022-07-18 16:49:19 +0300

    fix title

--- client_app/lib/pages/home_page.dart
+++ client_app/lib/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hogwarts faculty clock'),
+        title: const Text('Hogwarts houses clock'),
         centerTitle: true,
       ),
       body: Container(
