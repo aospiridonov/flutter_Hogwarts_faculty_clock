fd9276dd4dc5eeaa2ce042cb5d487b631b31bfbf
aospiridonov <aospiridonov@gmail.com>
2022-07-24 18:04:10 +0300

    fix asset

--- client_app/lib/pages/home_page.dart
+++ client_app/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:client_app/pages/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = '/home';
@@ -9,33 +10,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          height: 200,
-          width: 400,
-          child: Card(
-            margin: const EdgeInsets.all(10),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  OutlinedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(SchoolPage.routeName);
-                    },
-                    child: const Text('Hogwarts'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(DumbledorePage.routeName);
-                    },
-                    child: const Text('Dambldore'),
-                  ),
-                ],
+      body: Container(
+        color: Colors.lightBlue[100],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: SvgPicture.asset(
+                    'assets/images/Coat_of_arms_Hogwart_with_motto.svg'),
               ),
-            ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 200,
+                width: 400,
+                child: Card(
+                  elevation: 10,
+                  color: Colors.grey[100],
+                  margin: const EdgeInsets.all(10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(SchoolPage.routeName);
+                          },
+                          child: const Text('Hogwarts'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(DumbledorePage.routeName);
+                          },
+                          child: const Text('Dambldore'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
